634fa5c8fc9816469bdcc8b42ec84003caeda78a
A125X <91656458+A125X@users.noreply.github.com>
2022-03-15 19:51:40 +0300

    Update main.py
    Cleaned code for a bit

--- main.py
+++ main.py
@@ -1,31 +1,4 @@
-#    I'm trying to create a class of neural networks
-#
-#    output = bn + wn * ... * f(b2 + w1 * f(b1 + w1 * input))
-#        
-#    I have to provide to the model constructor information about the layers
-#    It have to be a list of amount of neurons in every layer, including an input and the output
-#
-#
-#
-#
-#
-import numpy as np
 import matplotlib.pyplot as plt
-
-# non-linear function(s?) and their(?) derivatives
-
-def relu(x):
-    return np.maximum(x, 0)
-
-def d_relu(x):
-    return (x >= 0).astype(float)
-
-#
-def correct_value(x):
-    x = np.array(x)
-    return x @ x.T
-
-# basically my class
 
 class NeuralNetwork:
 
@@ -54,10 +27,8 @@ class NeuralNetwork:
             self.weights.append(np.random.random((layers_list[i], layers_list[i + 1])))
             self.biases.append(np.random.random((layers_list[i + 1], 1)))
 
-    #learning
-    def train(self, alpha, trainings):
-
-        #refreshing
+    #functions for training
+    def __train_refreshing__(self):
         self.d_weights.clear()
         self.d_biases.clear()
         self.d_t.clear()
@@ -65,59 +36,109 @@ class NeuralNetwork:
         self.alpha = alpha
         self.trainings = trainings
 
+    def __backward__(self, output, y, err):
+
+        d_output = np.array([[2 * np.sum(output - y)]])
+
+        final_d_h = d_output
+        self.d_h.insert(0, final_d_h)
+
+        final_d_t = d_output
+        self.d_t.insert(0, final_d_t)
+
+        final_d_b = final_d_t
+        self.d_biases.insert(0, final_d_b)
+
+        final_d_w = self.h[-2].T @ final_d_t
+        self.d_weights.insert(0, final_d_w)
+
+        for i in range(1, len(self.weights)):
+
+            d_h = self.d_t[0] @ self.weights[-i].T
+            self.d_h.insert(0, d_h)
+
+            d_t = self.d_h[0] * self.__d_relu__(self.t[-i - 1])
+            self.d_t.insert(0, d_t)
+
+            d_b = d_t
+            self.d_biases.insert(0, d_b)
+
+            d_w = self.h[-i - 2].T @ self.d_t[0]
+            self.d_weights.insert(0, d_w)
+
+    def __update__(self):
+        for i in range(len(self.weights)):
+            self.weights[i] = self.weights[i] - self.d_weights[i] * self.alpha
+            self.biases[i] = self.biases[i] - self.d_biases[i].T * self.alpha
+
+    #ATTENTION: change random input back after experiments
+    def __random_input__(self):
+        input = 10 * np.random.random((self.layers_list[0], 1)).T
+        return input
+
+    def __correct_value__(self, input):
+        input = np.array(input)
+        return input @ input.T
+
+    #calculating output with providing optional parameters
+    def __train_calculate__(self, input):
+
+        temp = np.array(input)
+        self.t.clear()
+        self.h.clear()
+        self.t.append(input)
+        self.h.append(input)
+
+        for i in range(len(self.weights) - 1):
+            self.t.append(temp @ self.weights[i] + self.biases[i].T)
+            temp = self.__relu__(temp @ self.weights[i] + self.biases[i].T)
+            self.h.append(temp)
+
+        output = temp @ self.weights[len(self.weights) - 1] + self.biases[len(self.biases) - 1].T
+        self.t.append(output)
+        self.h.append(output)
+
+        return output
+
+    #activation functions
+    def __relu__(self, x):
+        return np.maximum(x, 0)
+
+    def __d_relu__(self, x):
+        return (x >= 0).astype(float)
+
+    #learning
+    def train(self, alpha, trainings):
+
+        #refreshing
+        self.__train_refreshing__()
+
         #learning cycle
         for i in range(self.trainings):
 
-            input = np.random.random((self.layers_list[0], 1)).T
-            y = correct_value(input)
+            #input and correct output calculation
+            input = self.__random_input__()
+            y = self.__correct_value__(input)
 
             #forward
-            output = self.calculate(input)
+            output = self.__train_calculate__(input)
+
+            #error calculation and appending to the error list
+            err = np.sum(np.linalg.matrix_power(output - y, 2))
+            self.err_list.append(err)
 
             #backward
-            err = np.sum(np.linalg.matrix_power(output - y, 2))
-            d_output = np.array([[2 * np.sum(output - y)]])
-
-            final_d_h = d_output
-            self.d_h.insert(0, final_d_h)
-
-            final_d_t = d_output
-            self.d_t.insert(0, final_d_t)
-
-            final_d_b = final_d_t
-            self.d_biases.insert(0, final_d_b)
-
-            final_d_w = self.h[-2].T @ final_d_t
-            self.d_weights.insert(0, final_d_w)
-
-            for i in range(1, len(self.weights)):
-
-                d_h = self.d_t[0] @ self.weights[-i].T
-                self.d_h.insert(0, d_h)
-
-                d_t = self.d_h[0] * d_relu(self.t[-i - 1])
-                self.d_t.insert(0, d_t)
-
-                d_b = d_t
-                self.d_biases.insert(0, d_b)
-
-                d_w = self.h[-i - 2].T @ self.d_t[0]
-                self.d_weights.insert(0, d_w)
+            self.__backward__(output, y, err)
 
             #update
-            for i in range(len(self.weights)):
-                self.weights[i] = self.weights[i] - self.d_weights[i] * self.alpha
-                self.biases[i] = self.biases[i] - self.d_biases[i].T * self.alpha
-
-            #add error
-            self.err_list.append(err)
+            self.__update__()
 
     #random input
     def show_random_test(self):
 
         input = np.random.random((self.layers_list[0], 1)).T
         output = self.calculate(input)
-        y = correct_value(input)
+        y = self.__correct_value__(input)
 
         err = np.sum(np.linalg.matrix_power(output - y, 2))
 
@@ -129,7 +150,7 @@ class NeuralNetwork:
     def show_determined_test(self, input):
 
         output = self.calculate(input)
-        y = correct_value(input)
+        y = self.__correct_value__(input)
 
         err = np.sum(np.linalg.matrix_power(output - y, 2))
 
@@ -141,35 +162,34 @@ class NeuralNetwork:
         plt.plot(self.err_list)
         plt.show()
 
-    #forward
+    #calculating output
     def calculate(self, input):
 
         temp = np.array(input)
-        self.t.clear()
-        self.h.clear()
-        self.t.append(input)
-        self.h.append(input)
 
         for i in range(len(self.weights) - 1):
-            self.t.append(temp @ self.weights[i] + self.biases[i].T)
-            temp = relu(temp @ self.weights[i] + self.biases[i].T)
-            self.h.append(temp)
+            temp = self.__relu__(temp @ self.weights[i] + self.biases[i].T)
 
         output = temp @ self.weights[len(self.weights) - 1] + self.biases[len(self.biases) - 1].T
-        self.t.append(output)
-        self.h.append(output)
 
         return output
-        
-layers = [2, 5, 5, 1]
+  
+#in this particular case I have some interesting results: network in attempts to optimize
+#it's behavior stuck at the only one specific output
+
+layers = [2, 4, 4, 4, 1]
 input = [3, 5]
-alpha = 0.001
-trainings = 10000
+alpha = 0.01
+trainings = 20000
 
 model = NeuralNetwork(layers)
 model.train(alpha, trainings)
 
 for i in range(10):
     model.show_random_test()
-#model.show_determined_test(input)
+
+print()
+
+model.show_determined_test(input)
+
 model.show_error()
