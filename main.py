3a8bd5c5019fcc8cbcb828de5fc2aa106458ad59
Alexander Shatalin <alexander.andr.shat@gmail.com>
2022-03-30 00:21:14 +0300

    Update main.py
    Now graph provide more information to the user, such as results on the test dataset, error per item in batch and legend. Also I added more activation functions: sigmoid and tanh.

--- main.py
+++ main.py
@@ -1,15 +1,64 @@
+#    I'm using this notation:
+#
+#    output = bn + wn * ... * f(b2 + w1 * f(b1 + w1 * input))
+
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
 import random
 
+#normalization function
+def softmax(x):
+    expo = np.exp(x)
+    expo_sum = np.sum(np.exp(x))
+    return expo/expo_sum
+
+#activation functions and their derivatives
+def relu(x):
+    return np.maximum(x, 0)
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+def tanh(x):
+    return (np.exp(x) - np.exp(-x)) / (np.exp(x) + np.exp(-x))
+
+def d_relu(x):
+    return (x >= 0).astype(float)
+
+def d_sigmoid(x):
+    s = sigmoid(x)
+    return s * (1 - s)
+
+def d_tanh(x):
+    return 1 - tanh(x) ** 2
+
+#loss functions and their derivatives
+def mse(output, y):
+    err = 0
+    for j in range(len(output)):
+        err += np.sum(np.square(output[j] - y[j]))
+    return np.array(err)
+
+def cross_entropy(output, y):
+    pass
+
+def d_mse(output, y):
+    d_error = []
+    for j in range(len(output)):
+        d_error.append(2 * (output[j] - y[j]))
+    return np.array(d_error)
+
+def d_cross_entropy(output, y):
+    pass
+
+#warning: here i use d_var as the name of derivative, not differential
 class NeuralNetwork:
 
     #сonstructor
-    def __init__(self, layers_list):
+    def __init__(self, layers_list, activation, loss, normalize = None):
 
         self.alpha = 0
-        self.trainings = 0
-        self.epochs = 0
         self.current_training = 0
         self.d_weights = []
         self.d_biases = []
@@ -17,29 +66,46 @@ class NeuralNetwork:
         self.d_h = []
         self.t = []
         self.h = []
-        self.dataset = []
+        self.test_err_list = []
         self.train_err_list = []
-        self.layers_list = layers_list
         self.weights = []
         self.biases = []
+        self.activation = activation
+        self.loss = loss
 
         #optimal type of weights & biases initialization
         for i in range(len(layers_list) - 1):
             self.weights.append((np.random.rand(layers_list[i], layers_list[i + 1]) - 0.5) * 2 * np.sqrt(1 / layers_list[i]))
             self.biases.append((np.random.rand(layers_list[i + 1], 1) - 0.5) * 2 * np.sqrt(1 / layers_list[i]))
+        
+    def __activation__(self, x):
+        if self.activation == 'relu':
+            return relu(x)
+        elif self.activation == 'sigmoid':
+            return sigmoid(x)
+        elif self.activation == 'tanh':
+            return tanh(x)
+
+    def __d_activation__(self, x):
+        if self.activation == 'relu':
+            return d_relu(x)
+        elif self.activation == 'sigmoid':
+            return d_sigmoid(x)
+        elif self.activation == 'tanh':
+            return d_tanh(x)
 
     #error calculation
-    def __err_calc__(self, output, y):
-        err = 0
-        for j in range(len(output)):
-            err = err + np.sum(np.square(output[j] - y[j]))
-        return np.array(err)
+    def __error__(self, output, y):
+        if self.loss == 'mse':
+            return mse(output, y)
+        elif self.loss == 'cross entropy':
+            return cross_entropy(output, y)
 
-    def __d_output_calc__(self, output, y):
-        d_out = []
-        for j in range(len(output)):
-            d_out.append(2 * (output[j] - y[j]))
-        return np.array(d_out)
+    def __d_error_d_output__(self, output, y):
+        if self.loss == 'mse':
+            return d_mse(output, y)
+        elif self.loss == 'cross entropy':
+            return d_cross_entropy(output, y)
 
     #functions for training
     def __train_refreshing__(self):
@@ -47,11 +113,12 @@ class NeuralNetwork:
         self.d_biases.clear()
         self.d_t.clear()
         self.d_h.clear()
-        self.alpha = alpha
+        self.train_err_list = []
+        self.test_err_list = []
 
     def __backward__(self, output, y, err):
 
-        d_output = self.__d_output_calc__(output, y)
+        d_output = self.__d_error_d_output__(output, y)
 
         final_d_h = d_output
         self.d_h.insert(0, final_d_h)
@@ -59,7 +126,7 @@ class NeuralNetwork:
         final_d_t = d_output
         self.d_t.insert(0, final_d_t)
 
-        final_d_b = np.sum(final_d_t, axis=0, keepdims=True)
+        final_d_b = np.sum(final_d_t, axis=0, keepdims = True)
         self.d_biases.insert(0, final_d_b)
 
         final_d_w = self.h[-2].T @ final_d_t
@@ -70,10 +137,10 @@ class NeuralNetwork:
             d_h = self.d_t[0] @ self.weights[-i].T
             self.d_h.insert(0, d_h)
 
-            d_t = self.d_h[0] * self.__d_relu__(self.t[-i - 1])
+            d_t = self.d_h[0] * self.__d_activation__(self.t[-i - 1])
             self.d_t.insert(0, d_t)
 
-            d_b = np.sum(d_t, axis=0, keepdims=True)
+            d_b = np.sum(d_t, axis=0, keepdims = True)
             self.d_biases.insert(0, d_b)
 
             d_w = self.h[-i - 2].T @ self.d_t[0]
@@ -84,13 +151,7 @@ class NeuralNetwork:
             self.weights[i] = self.weights[i] - self.d_weights[i] * self.alpha
             self.biases[i] = self.biases[i] - self.d_biases[i].T * self.alpha
 
-    def __train_input__(self):
-        return np.random.random((self.layers_list[0], 1)).T
-
-    def __train_correct_value__(self, input):
-        return dataset[self.current_training][1]
-
-    #calculating output with providing optional parameters
+    #calculating output with providing optional parameters: list of t and h
     def __train_calculate__(self, input):
 
         temp = np.array(input)
@@ -101,7 +162,7 @@ class NeuralNetwork:
 
         for i in range(len(self.weights) - 1):
             self.t.append(temp @ self.weights[i] + self.biases[i].T)
-            temp = self.__relu__(temp @ self.weights[i] + self.biases[i].T)
+            temp = self.__activation__(temp @ self.weights[i] + self.biases[i].T)
             self.h.append(temp)
 
         output = temp @ self.weights[len(self.weights) - 1] + self.biases[len(self.biases) - 1].T
@@ -110,57 +171,57 @@ class NeuralNetwork:
 
         return output
 
-    #activation functions
-    def __relu__(self, x):
-        return np.maximum(x, 0)
-
-    def __d_relu__(self, x):
-        return (x >= 0).astype(float)
-
     #learning
-    def train(self, alpha, trainings, epochs, batch_size, dataset = None):
+    def train(self, alpha, trainings, epochs, batch_size, dataset = None, test_dataset = None):
 
         #refreshing
         self.__train_refreshing__()
 
         #initializing
-        self.trainings = trainings
-        self.epochs = epochs
-        if dataset is not None:
-            self.dataset = dataset
-        else:
-            self.dataset = []
+        self.alpha = alpha
 
         #learning cycle
-        for i in range(self.epochs):
+        for i in range(epochs):
 
             random.shuffle(dataset)
 
-            for j in range(self.trainings):
+            for j in range(trainings):
 
                 self.current_training = j
 
                 #input and correct output calculation
-                if dataset is not None:
-                    batch_x, batch_y = zip(*dataset[j * batch_size : j * batch_size + batch_size])
-                    input = np.concatenate(batch_x, axis=0)
-                    y = []
-                    for i in batch_y:
-                        y.append([i])
-                    y = np.array(batch_y)
-                else:
-                    input = self.__train_input__()
-                    y = self.__train_correct_value__(input)
+                batch_x, batch_y = zip(*dataset[j * batch_size : j * batch_size + batch_size])
+                input = np.concatenate(batch_x, axis=0)
+                y = []
+                for i in batch_y:
+                    y.append([i])
+                y = np.array(batch_y)
 
                 #forward
                 output = self.__train_calculate__(input)
     
                 #error calculation and appending to the error list
-                err = self.__err_calc__(output, y)
-                self.train_err_list.append(err)
+
+                #test data calculation
+                if test_dataset is not None:
+                    test_batch_x, test_batch_y = zip(*test_dataset[j * len(test_dataset) : j * len(test_dataset) + len(test_dataset)])
+                    test_input = np.concatenate(test_batch_x, axis=0)
+                    test_y = []
+                    for i in test_batch_y:
+                        test_y.append([i])
+                    test_y = np.array(test_batch_y)
+                    test_output = self.calculate(test_input)
+
+                    #test error calculation and adding average error per item in the error list
+                    err_test = self.__error__(test_output, test_y)
+                    self.test_err_list.append(err_test / len(test_dataset))
+
+                #train error calculation and adding average error per item in the error list
+                err_train = self.__error__(output, y)
+                self.train_err_list.append(err_train / len(dataset))
 
                 #backward
-                self.__backward__(output, y, err)
+                self.__backward__(output, y, err_train)
 
                 #update
                 self.__update__()
@@ -173,19 +234,27 @@ class NeuralNetwork:
             y = [dataset_i[1]]
             output = self.calculate(input)
 
-            err = self.__err_calc__(output, y)
+            err = self.__error__(output, y)
         else:
             output = self.calculate(input)
             y = self.__correct_value__(input)
 
-            err = self.__err_calc__(output, y)
+            err = self.__error__(output, y)
 
         print('Neural network output: ' + str(output))
         print('Correct output: ' + str(y))
         print('Error: ' + str(err))
 
+    #average error
     def show_error(self):
-        plt.plot(self.train_err_list)
+        fig, ax = plt.subplots()
+        ax.plot(self.train_err_list, label='Train error')
+        ax.plot(self.test_err_list, label='Test error')
+        ax.grid()
+        ax.legend()
+        plt.suptitle('Average error per item in batch')
+        plt.xlabel('Trainings')
+        plt.ylabel('Error')
         plt.show()
 
     #calculating output
@@ -194,40 +263,97 @@ class NeuralNetwork:
         temp = np.array(input)
 
         for i in range(len(self.weights) - 1):
-            temp = self.__relu__(temp @ self.weights[i] + self.biases[i].T)
+            temp = self.__activation__(temp @ self.weights[i] + self.biases[i].T)
 
-        output = temp @ self.weights[len(self.weights) - 1] + self.biases[len(self.biases) - 1].T
+        output = temp @ self.weights[-1] + self.biases[-1].T
 
         return output
 
 from sklearn import datasets
 
-
+'''
 #iris
+
+#obtaining dataset
 iris = datasets.load_iris()
 dataset = [(iris.data[i][None, ...], iris.target[i]) for i in range(len(iris.target))]
 random.shuffle(dataset)
 
+#using part of the original data to train our neural network
 dataset_trainings_len = len(dataset) // 10 * 9
 dataset_training = []
 for i in range(dataset_trainings_len):
     dataset_training.append(dataset[i])
 
-dataset_test = []
+#and other part to test our network
+test_dataset = []
 for i in range(dataset_trainings_len, len(dataset)):
-    dataset_test.append(dataset[i])
+    test_dataset.append(dataset[i])
 
+#providing some hyperparameters to the network
 layers = [4, 10, 1]
-alpha = 0.0002
+alpha = 0.00005
 batch_size = len(dataset_training)
 trainings = len(dataset_training) //  batch_size
 epochs = 1000
 
+#creating and training the model
 model = NeuralNetwork(layers)
-model.train(alpha, trainings, epochs, batch_size, dataset_training)
+model.train(alpha, trainings, epochs, batch_size, dataset_training, test_dataset)
 
+#testing our model using test dataset
 for i in range(10):
-    model.show_determined_test(dataset_i = dataset_test[i])
+    model.show_determined_test(dataset_i = test_dataset[i])
+    print()
+
+model.show_error()
+
+for i in range(3):
+    print()
+'''
+
+#digits
+
+#obtaining dataset
+digits = datasets.load_digits()
+arr_out = []
+for i in range(10):
+    out = []
+    for j in range(10):
+        if (i == j):
+            out.append(1)
+        else:
+            out.append(0)
+    arr_out.append(out)
+
+dataset = [(digits.data[i][None, ...], arr_out[digits.target[i]]) for i in range(len(digits.target))]
+random.shuffle(dataset)
+
+#using part of the original data to train our neural network
+dataset_trainings_len = len(dataset) // 10 * 9
+dataset_training = []
+for i in range(dataset_trainings_len):
+    dataset_training.append(dataset[i])
+
+#and other part to test our network
+test_dataset = []
+for i in range(dataset_trainings_len, len(dataset)):
+    test_dataset.append(dataset[i])
+
+#providing some hyperparameters to the network
+layers = [64, 32, 16, 10]
+alpha = 0.00003
+batch_size = len(dataset_training)
+trainings = len(dataset_training) //  batch_size
+epochs = 1000
+
+#creating and training the model
+model = NeuralNetwork(layers, 'tanh', 'mse')
+model.train(alpha, trainings, epochs, batch_size, dataset_training, test_dataset)
+
+#testing our model using test dataset
+for i in range(10):
+    model.show_determined_test(dataset_i = test_dataset[i])
     print()
 
 model.show_error()
